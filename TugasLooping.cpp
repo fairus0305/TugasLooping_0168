409b9f96b0cb66fb9b4362c74a9ff264ae6329a8
fairus0305 <ruszezzy@gmail.com>
2026-03-30 17:48:24 +0700

    menmabhakan function untuk cek bilangan prima

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -4,3 +4,21 @@ using namespace std;
 // variabel global
 int n;
 int pilihan;
+
+// Function untuk cek bilangan prima
+bool Prima(int x)
+{
+    if (x <= 1)
+        return false;
+
+    int i = 2;
+    while (i < x)
+    {
+        if (x % i == 0)
+        {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
